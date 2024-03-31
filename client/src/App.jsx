1b224c884c6f05bc2dbdf63a47bfadbebc71a950
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-03-31 08:24:18 +0530

    added header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/home/Home.jsx";
-import Header from "./components/layout/Header/Header.jsx";
 import Courses from "./components/courses/Courses.jsx";
+import Header from "./components/layout/header/Header.jsx";
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/courses" element={<Courses />}></Route>
--- client/src/components/layout/header/Header.jsx
+++ client/src/components/layout/header/Header.jsx
@@ -1,6 +1,106 @@
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  HStack,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import ColorModeSwitcher from "../../../ColorModeSwitcher";
+import { RiDashboardFill, RiLogoutBoxLine, RiMenu5Fill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+
 const Header = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const isAuthenticated = true;
+  const user = { role: "admin" };
+
+  const logoutHandler = () => {};
+
   return (
-    <div>Header</div>
-  )
+    <>
+      <ColorModeSwitcher />
+      <Button
+        onClick={onOpen}
+        colorScheme="yellow"
+        width={12}
+        height={12}
+        rounded={"full"}
+        position={"fixed"}
+        top="6"
+        left="6"
+      >
+        <RiMenu5Fill />
+      </Button>
+      <Drawer placement="left" isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay backdropFilter={"blur(2px)"} />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth={"1px"}>COURSE BUNDLER</DrawerHeader>
+          <DrawerBody>
+            <VStack spacing={4} alignItems={"flex-start"}>
+              <LinkButtons url="/" title="Home" />
+              <LinkButtons url="/courses" title="Browse Courses" />
+              <LinkButtons url="/request" title="Request a Course" />
+              <LinkButtons url="/contact" title="Contact Us" />
+              <LinkButtons url="/about" title="About" />
+              <HStack
+                justifyContent={"space-evenly"}
+                position={"absolute"}
+                bottom={"2rem"}
+                width={"80%"}
+              >
+                {isAuthenticated ? (
+                  <>
+                    <VStack>
+                      <HStack>
+                        <Link to="/profile">
+                          <Button variant={"ghost"} colorScheme="yellow">
+                            Profile
+                          </Button>
+                        </Link>
+                        <Button onClick={logoutHandler} variant={"ghost"}>
+                          <RiLogoutBoxLine />
+                          logout
+                        </Button>
+                      </HStack>
+                      {user && user.role === "admin" && (
+                        <Link to="/admin/dashboard">
+                          <Button variant={"ghost"} colorScheme="purple">
+                            <RiDashboardFill style={{ margin: "4px" }} />
+                            Dashboard
+                          </Button>
+                        </Link>
+                      )}
+                    </VStack>
+                  </>
+                ) : (
+                  <>
+                    <Link to="/login">
+                      <Button colorScheme="yellow">Login</Button>
+                    </Link>
+                    <p>Or</p>
+                    <Link to="/register">
+                      <Button colorScheme="yellow">SignUp</Button>
+                    </Link>
+                  </>
+                )}
+              </HStack>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+export default Header;
+
+function LinkButtons({ url = "/", title = "Home" }) {
+  return (
+    <Link to={url}>
+      <Button variant={"ghost"}>{title}</Button>
+    </Link>
+  );
 }
-export default Header
