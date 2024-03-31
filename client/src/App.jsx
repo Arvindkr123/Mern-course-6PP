6a2ce3b77237bac11674a7081ad34f9f8ae6aa6a
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-03-31 09:53:32 +0530

    footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/home/Home.jsx";
 import Courses from "./components/courses/Courses.jsx";
 import Header from "./components/layout/header/Header.jsx";
+import Footer from "./components/layout/footer/Footer.jsx";
 const App = () => {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="/courses" element={<Courses />}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
--- client/src/components/layout/footer/Footer.jsx
+++ client/src/components/layout/footer/Footer.jsx
@@ -1,6 +1,40 @@
+import { Box, HStack, Heading, Stack, VStack } from "@chakra-ui/react";
+import {
+  TiSocialYoutubeCircular,
+  TiSocialInstagramCircular,
+} from "react-icons/ti";
+import { DiGithub } from "react-icons/di";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-export default Footer
+    <Box padding={4} bg="blackAlpha.900" minH={"10vh"}>
+      <Stack direction={["column", "row"]}>
+        <VStack alignItems={["center", "flex-start"]} width={"100%"}>
+          <Heading children="All Rights Reserved" color={"white"} />
+          <Heading
+            children="@Arvind Kumar"
+            color={"yellow.400"}
+            fontFamily={"body"}
+            size={"sm"}
+          />
+        </VStack>
+        <HStack
+          spacing={["2", "10"]}
+          justifyContent={"center"}
+          color={"white"}
+          fontSize={50}
+        >
+          <a href="https://www.youtube.com" target="_blank">
+            <TiSocialYoutubeCircular />
+          </a>
+          <a href="https://www.instagram.com" target="_blank">
+            <TiSocialInstagramCircular />
+          </a>
+          <a href="https://github.com/Arvindkr123" target="_blank">
+            <DiGithub />
+          </a>
+        </HStack>
+      </Stack>
+    </Box>
+  );
+};
+export default Footer;
